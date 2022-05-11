4ef676791b0e32d161ada7235a2bc33a9afea570
Rui Guo <guojiarui@gmail.com>
2022-05-10 22:29:51 -0700

    js updates

--- js/app.js
+++ js/app.js
@@ -1,47 +1,56 @@
 'use strict';
 
 let answerOne = prompt('what\'s your name?');
-alert('welcome ${answerOne}!');
+alert('welcome ' + answerOne +'!');
 console.log(answerOne);
 
-//if conditional based confirm command
+//if conditional based prompt command
 let answerTwo = prompt('Are you ready to play my guessing game?').toLowerCase();
+
 if (answerTwo === 'yes' || answerTwo === 'y'){
   alert('let\'s play!');
+//console.log('let\'s play!');
 } else if(answerTwo === 'no' || answerTwo === 'n'){
   alert('sorry you have to play, it is madantory.' );}
-// console.log('let\'s play!');
 
+let answerThree= prompt('do I have a cat?').toLowerCase();
 
-// // logical operator example
-// let myBool = true;
-// let anotherBool = false;
-// let thirdBool = true;
+if (answerThree === 'yes' || answerThree === 'y'){
+  alert('bingo, I do have a cat, her name is Gigi.');
+//console.log('bingo, I do have a cat, her name is Gigi.');
+} else if(answerThree === 'no' || answerThree === 'n'){
+  alert('emm... you wanna change your answer?' );}
 
-// // And = both conditions have to be true
-// if (myBool && thirdBool) {
-//   console.log('myBool and thirdBool were both true');
-// } // only one of these have to be true
-// else if (myBool || anotherBool) {
-//   console.log('My bool or anotherBool was true!');
-// } // only thirdBool can be true
-// else if (thirdBool) {
-//   console.log('myBool and anotherBool were false, but thirdBool was true.');
-// } // none of the above conditions were true
-// else {
-//   console.log('Were any of my variables true?');
-// }
+let answerFour= prompt('do I like outdoor activities?').toLowerCase();
 
-// let color = prompt('what is your favorite color');
+if (answerFour === 'yes' || answerFour === 'y'){
+  alert('It is a sure thing! I love outdoors.');
+// console.log('It is a sure thing! I love outdoors.');
+} else if(answerFour === 'no' || answerFour === 'n'){
+  alert('but I do like outdoors.' );}
 
-// // convert the variable of color to lower case and evaluate it
-// switch (color.toLowerCase()) {
-// case 'red':
-//   console.log('your favorite color was red!');
-//   break;
-// case 'blue':
-//   console.log('your favorite color was blue!');
-//   break;
-// default:
-//   console.log('i don\'t know what your favorite color was ¯\\_(ツ)_/¯');
-// }
+let answerFive= prompt('Am I married?').toLowerCase();
+
+if (answerFour === 'yes' || answerFive === 'y'){
+  alert('Yes, you are right, I am married.');
+// console.log('Yes, you are right, I am married.');
+} else if(answerFive === 'no' || answerFive === 'n'){
+  alert('right answer is opposite.' );}
+
+let color = prompt('what is my favorite color?');
+
+//convert the variable of color to lower case and evaluate it
+switch (color.toLowerCase()) {
+case 'red':
+  alert('my favorite color was red!');
+  break;
+case 'blue':
+  alert('my favorite color was blue!');
+  break;
+default:
+  alert('you don\'t know what my favorite color was ¯\\_(ツ)_/¯');
+
+// console.log('my favorite colors are blue and Red.');
+}
+
+confirm(answerOne +', you know me better now, can we make friends?');
