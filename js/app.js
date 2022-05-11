24fb1ef7377084d821eaf396287015263194b2b2
Rui Guo <guojiarui@gmail.com>
2022-05-10 18:26:35 -0700

    updates

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict'
+
+//if conditional based confirm command
+let answer1 = prompt('what\'s your name?');
+alert('welcome ${answer1}!');
+
+let answer2 = confirm('Are you ready to play my guessing game?')
+if (answer2 === true){console.log('let\'s play!')}
+alert('let\'s play!')
+
+// logical operator example
+let myBool = true;
+let anotherBool = false;
+let thirdBool = true;
+
+// And = both conditions have to be true
+if (myBool && thirdBool) {
+  console.log('myBool and thirdBool were both true');
+} // only one of these have to be true
+else if (myBool || anotherBool) {
+  console.log('My bool or anotherBool was true!');
+} // only thirdBool can be true
+else if (thirdBool) {
+  console.log('myBool and anotherBool were false, but thirdBool was true.');
+} // none of the above conditions were true
+else {
+  console.log('Were any of my variables true?');
+}
+
+let color = prompt('what is your favorite color');
+
+// convert the variable of color to lower case and evaluate it
+switch (color.toLowerCase()) {
+case 'red':
+  console.log('your favorite color was red!');
+  break;
+case 'blue':
+  console.log('your favorite color was blue!');
+  break;
+default:
+  console.log('i don\'t know what your favorite color was ¯\\_(ツ)_/¯');
+}
