c413dd20ef9a1350eb55fa400425d7f37d9eff9f
Stephanie Hill <stephni.hill@gmail.com>
2022-05-12 13:47:57 -0700

    add function answerThree

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-let score =0;
+let score = 0;
 
 let answerOne = prompt('what\'s your name?');
 alert(`welcome ${answerOne}!`);
@@ -8,68 +8,75 @@ console.log(answerOne);
 //if conditional based prompt command
 let answerTwo = prompt('Are you ready to play my guessing game?').toLowerCase();
 
-if (answerTwo === 'yes' || answerTwo === 'y'){
+if (answerTwo === 'yes' || answerTwo === 'y') {
   alert('let\'s play!');
-//console.log('let\'s play!');
-} else if(answerTwo === 'no' || answerTwo === 'n'){
-  alert('sorry you have to play, it is madantory.' );}
+  //console.log('let\'s play!');
+} else if (answerTwo === 'no' || answerTwo === 'n') {
+  alert('sorry you have to play, it is madantory.');
+}
+function answerThree() {
+  let answerThree = prompt('do I have a cat?').toLowerCase();
 
-let answerThree= prompt('do I have a cat?').toLowerCase();
+  if (answerThree === 'yes' || answerThree === 'y') {
+    alert('bingo, I do have a cat, her name is Gigi.');
+    score++;
+    //console.log('bingo, I do have a cat, her name is Gigi.');
+  } else if (answerThree === 'no' || answerThree === 'n') {
+    alert('emm... you wanna change your answer?');
+  }
+}
+answerThree();
 
-if (answerThree === 'yes' || answerThree === 'y'){
-  alert('bingo, I do have a cat, her name is Gigi.');
-  score ++;
-//console.log('bingo, I do have a cat, her name is Gigi.');
-} else if(answerThree === 'no' || answerThree === 'n'){
-  alert('emm... you wanna change your answer?' );}
 
-let answerFour= prompt('do I like outdoor activities?').toLowerCase();
+let answerFour = prompt('do I like outdoor activities?').toLowerCase();
 
-if (answerFour === 'yes' || answerFour === 'y'){
+if (answerFour === 'yes' || answerFour === 'y') {
   alert('It is a sure thing! I love outdoors.');
-  score ++;
-// console.log('It is a sure thing! I love outdoors.');
-} else if(answerFour === 'no' || answerFour === 'n'){
-  alert('but I do like outdoors.' );}
+  score++;
+  // console.log('It is a sure thing! I love outdoors.');
+} else if (answerFour === 'no' || answerFour === 'n') {
+  alert('but I do like outdoors.');
+}
 
-let answerFive= prompt('Am I married?').toLowerCase();
+let answerFive = prompt('Am I married?').toLowerCase();
 
-if (answerFour === 'yes' || answerFive === 'y'){
+if (answerFour === 'yes' || answerFive === 'y') {
   alert('Yes, you are right, I am married.');
-  score ++;
-// console.log('Yes, you are right, I am married.');
-} else if(answerFive === 'no' || answerFive === 'n'){
-  alert('right answer is opposite.' );}
+  score++;
+  // console.log('Yes, you are right, I am married.');
+} else if (answerFive === 'no' || answerFive === 'n') {
+  alert('right answer is opposite.');
+}
 
 let color = prompt('what is my favorite color?');
 
 //convert the variable of color to lower case and evaluate it
 
 switch (color.toLowerCase()) {
-case 'red':
-  alert('my favorite color was red!');
-  break;
-case 'blue':
-  alert('my favorite color was blue!');
-  break;
-default:
-  alert('you don\'t know what my favorite color was ¯\\_(ツ)_/¯');
-  score ++;
-// console.log('my favorite colors are blue and Red.');
+  case 'red':
+    alert('my favorite color was red!');
+    break;
+  case 'blue':
+    alert('my favorite color was blue!');
+    break;
+  default:
+    alert('you don\'t know what my favorite color was ¯\\_(ツ)_/¯');
+    score++;
+  // console.log('my favorite colors are blue and Red.');
 }
 
 let answerSix = prompt('guess what is my secret number?');
 let i = 0;
 
-for (i = 0; i < 4; i ++){
+for (i = 0; i < 4; i++) {
   if (answerSix > 8) {
     alert('too high!');
     answerSix = prompt('guess what is my secret number?');
-  }else if (answerSix < 8 ){
+  } else if (answerSix < 8) {
     alert('too low!');
     answerSix = prompt('guess what is my secret number?');
-  }else {
-    alert('bingo! how did you do that?!'); score ++;
+  } else {
+    alert('bingo! how did you do that?!'); score++;
     break;
   }
 }
@@ -78,11 +85,11 @@ console.log('');
 
 let arrLucky = ['a', 'i', 'd', 'v', 'g'];
 
-for (let i = 0; i < 6; i ++){
-  let answerSeven =prompt('can you hit one of my five lucky letters?');
-  for (let j = 0; j < 5; j ++){
-    if (answerSeven === arrLucky[j]){
-      alert('you hit one of my lucky letters!'); score ++;
+for (let i = 0; i < 6; i++) {
+  let answerSeven = prompt('can you hit one of my five lucky letters?');
+  for (let j = 0; j < 5; j++) {
+    if (answerSeven === arrLucky[j]) {
+      alert('you hit one of my lucky letters!'); score++;
       i = 6;
       break;
     }
