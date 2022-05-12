8aad3238767826a5c7f7a94f53a135a0d3fec8c6
Rui Guo <guojiarui@gmail.com>
2022-05-11 22:00:38 -0700

    html and css updated

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+let score =0;
 
 let answerOne = prompt('what\'s your name?');
 alert(`welcome ${answerOne}!`);
@@ -17,6 +18,7 @@ let answerThree= prompt('do I have a cat?').toLowerCase();
 
 if (answerThree === 'yes' || answerThree === 'y'){
   alert('bingo, I do have a cat, her name is Gigi.');
+  score ++;
 //console.log('bingo, I do have a cat, her name is Gigi.');
 } else if(answerThree === 'no' || answerThree === 'n'){
   alert('emm... you wanna change your answer?' );}
@@ -25,6 +27,7 @@ let answerFour= prompt('do I like outdoor activities?').toLowerCase();
 
 if (answerFour === 'yes' || answerFour === 'y'){
   alert('It is a sure thing! I love outdoors.');
+  score ++;
 // console.log('It is a sure thing! I love outdoors.');
 } else if(answerFour === 'no' || answerFour === 'n'){
   alert('but I do like outdoors.' );}
@@ -33,6 +36,7 @@ let answerFive= prompt('Am I married?').toLowerCase();
 
 if (answerFour === 'yes' || answerFive === 'y'){
   alert('Yes, you are right, I am married.');
+  score ++;
 // console.log('Yes, you are right, I am married.');
 } else if(answerFive === 'no' || answerFive === 'n'){
   alert('right answer is opposite.' );}
@@ -50,6 +54,7 @@ case 'blue':
   break;
 default:
   alert('you don\'t know what my favorite color was ¯\\_(ツ)_/¯');
+  score ++;
 // console.log('my favorite colors are blue and Red.');
 }
 
@@ -64,23 +69,26 @@ for (i = 0; i < 4; i ++){
     alert('too low!');
     answerSix = prompt('guess what is my secret number?');
   }else {
-    alert('bingo! how did you do that?');
+    alert('bingo! how did you do that?!'); score ++;
     break;
   }
 }
-
 alert('my secret number is 8');
+console.log('');
 
+let arrLucky = ['a', 'i', 'd', 'v', 'g'];
 
+for (let i = 0; i < 6; i ++){
+  let answerSeven =prompt('can you hit one of my five lucky letters?');
+  for (let j = 0; j < 5; j ++){
+    if (answerSeven === arrLucky[j]){
+      alert('you hit one of my lucky letters!'); score ++;
+      i = 6;
+      break;
+    }
+  }
+}
+alert('my lucky letters are a, d, i, v, g.');
 
-
-
-
-
-
-
-
-
-
-confirm(`${answerOne}, you know me better now, can we make friends?`);
+confirm(`${answerOne}, you scored ${score} out of 7, you know me better now, can we make friends?`);
 
